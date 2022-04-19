06573c990b1dcc17d73305951aea4704ce920083
enjoyw <enjoywg@gmail.com>
2022-04-19 12:06:31 +0300

    fix pep8

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from ..models import SLICE_SIZE, Group, Post, Comment, Follow
+from ..models import SLICE_SIZE, Comment, Follow, Group, Post
 
 User = get_user_model()
 
